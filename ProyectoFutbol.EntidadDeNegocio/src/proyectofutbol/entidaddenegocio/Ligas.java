4bc1e22cbaa7449c96bd3952df7750fdc50a2a97
JosephHrndz <ah22003@esfe.agape.edu.sv>
2023-08-12 15:54:03 -0600

    Las entidades

--- ProyectoFutbol.EntidadDeNegocio/src/proyectofutbol/entidaddenegocio/Ligas.java
+++ ProyectoFutbol.EntidadDeNegocio/src/proyectofutbol/entidaddenegocio/Ligas.java
@@ -9,5 +9,52 @@ package proyectofutbol.entidaddenegocio;
  * @author MINEDUCYT
  */
 public class Ligas {
+    private int idLigas;
+    private String nombreLiga;
+    private String paisLiga;
+    private int top_aux;
+
+    public Ligas() {
+    }
+
+    public Ligas(int idLigas, String nombreLiga, String paisLiga, int top_aux) {
+        this.idLigas = idLigas;
+        this.nombreLiga = nombreLiga;
+        this.paisLiga = paisLiga;
+        this.top_aux = top_aux;
+    }
+
+    public int getIdLigas() {
+        return idLigas;
+    }
+
+    public void setIdLigas(int idLigas) {
+        this.idLigas = idLigas;
+    }
+
+    public String getNombreLiga() {
+        return nombreLiga;
+    }
+
+    public void setNombreLiga(String nombreLiga) {
+        this.nombreLiga = nombreLiga;
+    }
+
+    public String getPaisLiga() {
+        return paisLiga;
+    }
+
+    public void setPaisLiga(String paisLiga) {
+        this.paisLiga = paisLiga;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+    
     
 }
